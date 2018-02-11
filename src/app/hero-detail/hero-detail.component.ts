16d7cbd5f024c6b8bb9be30526989309072d6bfa
Joabe Costa <joabetc@hotmail.com>
2018-02-10 22:45:45 -0200

    Implementing getHero method

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -23,4 +23,9 @@ export class HeroDetailComponent implements OnInit {
   ngOnInit() {
   }
 
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
 }
