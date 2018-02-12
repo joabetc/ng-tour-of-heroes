492a575dc621f2f839650baba3b44cfe9c86098b
Joabe Costa <joabetc@hotmail.com>
2018-02-12 11:55:43 -0200

    Creating a method to add a new hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,6 +8,10 @@ import { Hero } from './hero/hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable()
 export class HeroService {
 
@@ -37,12 +41,16 @@ export class HeroService {
   }
   
   updateHero(hero: Hero): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    };
     return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
       tap(_ => this.log(`update hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
     );
   }
 
