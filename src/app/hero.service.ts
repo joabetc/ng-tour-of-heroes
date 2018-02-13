ea1ffcc8a6a373cdfe2762e4a6ca018faecc654e
Joabe Costa <joabetc@hotmail.com>
2018-02-13 19:04:50 -0200

    Grouping imports by its origin

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero/hero';
